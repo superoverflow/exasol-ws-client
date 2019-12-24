78e16e22bd247c5f2764a981d02005c05c69a49f
cyrus.tang <cyrus.tang@revolut.com>
2019-12-24 15:29:45 +0000

    try to show result from a table

--- exasol-ws-client/src/components/sqlQuery.js
+++ exasol-ws-client/src/components/sqlQuery.js
@@ -4,10 +4,58 @@ import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import { Row, Col } from 'react-bootstrap'
+import Table from './table'
+
 
 function SqlQuery(props) {
-    const [queryText, setQueryText] = useState("")
+    const [queryText, setQueryText] = useState("select * from finance.category_history limit 10;")
     const [resultText, setResultText] = useState("")
+
+    const columns = [{
+        Header: 'finance.category_history',
+        columns: [
+            {
+                Header: 'id',
+                accessor: 'id'
+            },
+            {
+                Header: 'category_id',
+                accessor: 'category_id'
+            },
+            {
+                Header: 'category',
+                accessor: 'category'
+            },
+            {
+                Header: 'c_match',
+                accessor: 'c_match'
+            },
+            {
+                Header: 'c_comment',
+                accessor: 'c_comment'
+            },
+            {
+                Header: 'bank',
+                accessor: 'bank'
+            },
+            {
+                Header: 'created_date',
+                accessor: 'created_date'
+            },
+        ]
+    }]
+
+    const data = [
+        {
+            id: '6da78178-ef91-42d8-adb9-0fb5a8123a6f',
+            category_id: 57,
+            category: 'Client Top-up',
+            c_match: 'abc',
+            c_comment: 'def',
+            bank: 'Barclays',
+            created_date: '2019-12-01'
+        }
+    ]
 
     return (
         <>
@@ -15,7 +63,7 @@ function SqlQuery(props) {
                 <Col xs={10}>
                     <Form.Control
                         type="text"
-                        placeholder="input your sql here"
+                        defaultValue={queryText}
                         onChange={e => setQueryText(e.target.value)}
                     />
                 </Col>
@@ -39,6 +87,8 @@ function SqlQuery(props) {
                     />
                 </Col>
             </Row>
+            <hr />
+            <Table columns={columns} data={data} />
         </>
     )
 }
